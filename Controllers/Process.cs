145e12cb04d116c6dc9d8e4f00597579e4af8073
Joe Doumit <joe.doumit@wilsonallen.com>
2022-10-12 09:09:19 +0300

    initial project

---
+++ Controllers/Process.cs
@@ -0,0 +1,22 @@
+﻿using Core.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Process : ControllerBase
+    {
+        private IProcessService ProcessService { get; set; }
+        public Process(IProcessService processService)
+        {
+            ProcessService = processService;
+        }
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            return await ProcessService.GetProcesses();
+        }
+    }
+}
