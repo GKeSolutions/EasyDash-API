d9e28c47abca9f02eb0ae31c5c981ec61cd15f04
joe.doumit@intapp.com <joe.doumit@intapp.com>
2023-02-15 14:42:27 +0200

    fix

--- Core/Model/Notification/Template.cs
+++ Core/Model/Notification/Template.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public bool IsActive { get; set; }
+        public bool IsDefault { get; set; }
         public string Description { get; set; }
         public int Type { get; set; }
         public int Priority { get; set; }
