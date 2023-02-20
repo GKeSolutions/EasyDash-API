bdac522e4e1a62dbe1cffb0c18e3363e2d433c34
joe.doumit@intapp.com <joe.doumit@intapp.com>
2023-02-20 11:02:35 +0200

    fix

--- Core/Service/JobService.cs
+++ Core/Service/JobService.cs
@@ -2,6 +2,7 @@
 using Core.Interface;
 using Core.Model.Notification;
 using Hangfire;
+using Microsoft.Extensions.Configuration;
 
 namespace Core.Service
 {
@@ -10,12 +11,14 @@ namespace Core.Service
         private IMissingTimeService MissingTimeService;
         private INotificationService NotificationService;
         private IDashboardService DashboardService;
+        private readonly IConfiguration Configuration;
 
-        public JobService(IMissingTimeService missingTimeService, INotificationService notificationService, IDashboardService dashboardService) 
+        public JobService(IMissingTimeService missingTimeService, INotificationService notificationService, IDashboardService dashboardService, IConfiguration configuration) 
         {
             MissingTimeService = missingTimeService;
             DashboardService= dashboardService;
             NotificationService= notificationService;
+            Configuration = configuration;
         }
         public bool AddJob(ScheduledNotification scheduledNotification)
         {
@@ -42,9 +45,7 @@ namespace Core.Service
                     EmailAddress = user.UserEmail,
                     CcContact = "gilbert.khoury@gkesolutions.com"//await MissingTimeService.GetCcContactEmailAddress(scheduledNotification.CcContact)
                 };
-                var tags = new Dictionary<string, string>();
-                tags["UserName"] = user.UserName;
-                tags["ProcessCaption"] = user.ProcessCaption;
+                var tags = BuildProcessTags(user.UserName, user.ProcessCaption, user.LastUpdated, user.ProcessItemId);
                 await NotificationService.SendEmailNotification(emailNotification, tags);
             }
 
@@ -61,13 +62,32 @@ namespace Core.Service
                     EmailAddress = user.EmailAddress,
                     CcContact = "gilbert.khoury@gkesolutions.com"//await MissingTimeService.GetCcContactEmailAddress(scheduledNotification.CcContact)
                 };
-                var tags = new Dictionary<string, string>();
-                tags["UserName"] = user.UserName;
-                //tags["ProcessCaption"] = user.ProcessCaption;
+                var tags = BuildMissingTimeTags(user.UserName, user.WeekStartDate.ToString(), user.WeeklyHoursRequired, user.WorkHrs);
                 await NotificationService.SendEmailNotification(emailNotification, tags);
             }
 
             return string.Empty;
+        }
+
+        private Dictionary<string, string> BuildProcessTags(string userName, string processCaption, string lastUpdated, Guid processItemId)
+        {
+            var tags = new Dictionary<string, string>();
+            tags["UserName"] = userName;
+            tags["ProcessCaption"] = processCaption;
+            tags["LastUpdated"] = lastUpdated;
+            tags["ProcessLink"] = "<a href=" + Configuration["InstanceConfiguration:BaseUrl"] + "Process/" + processItemId + "> Process Link</a> ";
+            return tags;
+        }
+
+        private Dictionary<string, string> BuildMissingTimeTags(string userName, string weekName, decimal requiredHours, int loggedHours)
+        {
+            var tags = new Dictionary<string, string>();
+            tags["UserName"] = userName;
+            tags["WeekName"] = weekName;
+            tags["MissingHours"] = (requiredHours - loggedHours).ToString();
+            tags["RequiredHours"] = requiredHours.ToString();
+            tags["LoggedHours"] = loggedHours.ToString();
+            return tags;
         }
     }
 }
