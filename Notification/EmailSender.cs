282ae4a9fce70e64b35667fc87931332c6d404f5
GKeSolutions <Gilbert.khoury@gkesolutions.com>
2023-03-03 00:02:54 +0200

    Fixed SMTP issue

--- Notification/EmailSender.cs
+++ Notification/EmailSender.cs
@@ -33,7 +33,9 @@ namespace Notification
         }
         private async Task<bool> Send(MimeMessage mailMessage, CancellationToken ct = default)
         {
+            
             using var client = new SmtpClient();
+            client.ServerCertificateValidationCallback = (s, c, h, e) => true;
             if (EmailConfig.UseSSL)
             {
                 await client.ConnectAsync(EmailConfig.SmtpServer, EmailConfig.Port, SecureSocketOptions.SslOnConnect, ct);
@@ -44,7 +46,7 @@ namespace Notification
             }
             //client.Connect(EmailConfig.SmtpServer, EmailConfig.Port, MailKit.Security.SecureSocketOptions.StartTls);
             //client.AuthenticationMechanisms.Remove("XOAUTH2");
-            await client.AuthenticateAsync(EmailConfig.UserName, EmailConfig.Password, ct);
+            //await client.AuthenticateAsync(EmailConfig.UserName, EmailConfig.Password, ct);
 
             await client.SendAsync(mailMessage, ct);
             await client.DisconnectAsync(true, ct);
