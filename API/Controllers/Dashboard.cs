65b1b66c5b09921e33e7ce2715352f260e155143
joe.doumit@intapp.com <joe.doumit@intapp.com>
2023-03-06 11:16:43 +0200

    added logged in username

--- API/Controllers/Dashboard.cs
+++ API/Controllers/Dashboard.cs
@@ -10,15 +10,20 @@ namespace API.Controllers
     public class Dashboard : ControllerBase
     {
         private IDashboardService ProcessService { get; set; }
+        private readonly IHttpContextAccessor HttpContextAccessor;
+        private readonly ILogger<Dashboard> Logger;
 
-        public Dashboard(IDashboardService processService)
+        public Dashboard(IDashboardService processService, IHttpContextAccessor httpContextAccessor, ILogger<Dashboard> logger)
         {
             ProcessService = processService;
+            HttpContextAccessor = httpContextAccessor;
+            Logger = logger;
         }
 
         [HttpGet]
         public async Task<IEnumerable<Core.Model.Dashboard.User.DashUser>> user()
         {
+            Logger.LogDebug("The windows username:" + HttpContextAccessor.HttpContext.User.Identity.Name);
             return await ProcessService.GetUsers();
         }
 
