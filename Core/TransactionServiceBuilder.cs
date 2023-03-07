55671c5127e533951aefcd4e5826eeb9020fa389
marc <marc.abouzeyd@intapp.com>
2023-03-07 17:03:49 +0200

    fixed ntlm

--- Core/TransactionServiceBuilder.cs
+++ Core/TransactionServiceBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using System.ServiceModel;
 using System.Text;
 using TransactionService;
 
@@ -8,21 +9,6 @@ namespace Core
     {
         private IConfiguration Configuration { get; set; }
         private TransactionService.TransactionServiceClient Api;
-
-        public TransactionServiceBuilder(IConfiguration configuration)
-        {
-            Api = new TransactionService.TransactionServiceClient();
-            Configuration= configuration;
-            Api.Endpoint.Address = new System.ServiceModel.EndpointAddress(Configuration["InstanceConfiguration:BaseUrl"]);
-        }
-
-        public async Task<string> Reassign(string processCode, Guid procItemId, Guid reassignToUserId)
-        {
-            var xml = BuildReassignXML(processCode, procItemId, reassignToUserId);
-            var result = await Api.ExecuteProcessAsync(xml, 1);
-            return result.Body.ToString();
-        }
-
         const string XML_C_ReassignSrv_GK = "<C_ReassignProcess_GK xmlns=\"http://elite.com/schemas/transaction/process/write/C_ReassignProcess_GK\">";
         const string XML_C_ReassignSrv_GK_END = "</C_ReassignProcess_GK>";
         const string XML_C_InializeReassignSrv_GK = "<Initialize xmlns=\"http://elite.com/schemas/transaction/object/write/C_ReassignProcess_GK\">";
@@ -39,6 +25,35 @@ namespace Core
         const string XML_C_ProcItemId_GK_END = "</ProcItemId>";
         const string XML_C_ReassignToUserId_GK = "<ReassignToUserId>";
         const string XML_C_ReassignToUserId_GK_END = "</ReassignToUserId>";
+
+        public TransactionServiceBuilder(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            var binding = new BasicHttpBinding(BasicHttpSecurityMode.TransportCredentialOnly);
+            binding.OpenTimeout = binding.CloseTimeout =
+            binding.SendTimeout = binding.ReceiveTimeout = TimeSpan.FromMinutes(1);
+            binding.MaxReceivedMessageSize = 20971520;
+            binding.MaxBufferPoolSize = binding.MaxBufferSize = 20971520;
+            binding.ReaderQuotas.MaxArrayLength =
+            binding.ReaderQuotas.MaxStringContentLength =
+            binding.ReaderQuotas.MaxBytesPerRead =
+            binding.ReaderQuotas.MaxNameTableCharCount = 2097152;
+            binding.TextEncoding = Encoding.UTF8;
+            binding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Ntlm;
+            binding.TransferMode = TransferMode.Buffered;
+            binding.AllowCookies = false;
+            Api = new TransactionService.TransactionServiceClient(binding, new System.ServiceModel.EndpointAddress(Configuration["InstanceConfiguration:TransactionServiceURL"]));
+            Api.ClientCredentials.Windows.AllowedImpersonationLevel = System.Security.Principal.TokenImpersonationLevel.Delegation;
+            Api.ClientCredentials.Windows.ClientCredential.UserName = "EliteAdmDev";
+            Api.ClientCredentials.Windows.ClientCredential.Password = "SnEvs7nd3C";
+        }
+
+        public async Task<string> Reassign(string processCode, Guid procItemId, Guid reassignToUserId)
+        {
+            var xml = BuildReassignXML(processCode, procItemId, reassignToUserId);
+            var result = await Api.ExecuteProcessAsync(xml, 1);
+            return result.Body.ToString();
+        }
         private static string BuildReassignXML(string processCode, Guid procItemId, Guid reassignToUserId)
         {
             StringBuilder sb = new StringBuilder();
