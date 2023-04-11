5e6cc4ab12dfbe1d16bef8b73dd24c480e479ca8
marc <marc.abouzeyd@intapp.com>
2023-04-11 10:58:36 +0300

    fixed notifyafterdays to accept null

--- Core/Model/Notification/ScheduledNotification.cs
+++ Core/Model/Notification/ScheduledNotification.cs
@@ -7,7 +7,7 @@
         public int NotificationTemplate { get; set; }
         public string CronExpression { get; set; }
         public int Scheduler { get; set; }
-        public int NotifyAfterDays { get; set; }
+        public int? NotifyAfterDays { get; set; }
         public int EventType { get; set; }
         public Guid ReassignTo { get; set; }
         public string CcContact { get; set; }
